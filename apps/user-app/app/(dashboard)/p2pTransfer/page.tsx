a5fb6d0b83127f562db2cf2eecc2c37012d663be
Rishu Raj <rishuraj1702@gmail.com>
2025-06-09 10:18:14 +0530

    SSR p2p page

--- apps/user-app/app/(dashboard)/p2pTransfer/page.tsx
+++ apps/user-app/app/(dashboard)/p2pTransfer/page.tsx
@@ -2,8 +2,16 @@ import { Card } from "@repo/ui/card";
 import { Center } from "@repo/ui/center";
 import prisma from "@repo/db/client";
 import UsersCard from "../../../components/UsersCard";
+import { getServerSession } from "next-auth";
+import { NEXT_AUTH } from "../../lib/auth";
 
 async function getUsers() {
+    const session = await getServerSession(NEXT_AUTH)
+    if(!session){
+        return {
+            msg:"Unauthorized"
+        }
+    }
     const ppl = await prisma.user.findMany()
     return ppl
 }
